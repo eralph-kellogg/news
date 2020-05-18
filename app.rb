f2c635a39da3cbc25dd33bc73e29f30e554e22c4
eralph-kellogg <emily.ralph@kellogg.northwestern.edu>
2020-05-18 17:51:47 +0000

    base functionality

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get "/" do
   long = -87.6722787
 
   units = "imperial" # or metric, whatever you like
-  key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
+  key = "a5b32757a3a702c145b1d71d578fcaa1" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
@@ -18,5 +18,29 @@ get "/" do
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
 
+  @current = "It is currently #{@forecast["current"]["temp"]} degrees Farenheit and #{@forecast["current"]["weather"][0]["description"]}"
+
+    @extended = []
+        for day in @forecast["daily"]
+            @extended << "A high of #{day["temp"]["max"]} deg F and #{day["weather"][0]["main"]}"
+        end
+
+
   ### Get the news
+
+  key2 = "1af0961e7182478ea1a8f6ecc2092f52"
+  url2 = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{key2}"
+  @news = HTTParty.get(url2).parsed_response.to_hash
+  
+  # news is now a Hash you can pretty print (pp) and parse for your output
+
+  @headlines = []
+  @links = []
+  for story in @news["articles"]
+    @headlines << "#{story["title"]}"
+    @links << "#{story["url"]}"
+  end
+  
+
+  view 'news'
 end
